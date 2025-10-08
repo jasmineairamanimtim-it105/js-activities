5bdac5237056b015462f8db17c3a25b446073b4f
jasmineairamanimtim-it105 <jasmineaira.manimtim@olivarezcollegetagaytay.edu.ph>
2025-10-08 11:18:01 +0800

    js-activity it 105

---
+++ script.js
@@ -0,0 +1,11 @@
+const PI = 3.14159;
+
+let radius = 5;
+//Calculate the area 
+
+let area = PI * radius * radius;
+
+console.log("The radius of the circle is: " + radius);
+console.log("The value of PI is: "+ PI);
+console.log("The area of the circle is: " + area);
+ 
